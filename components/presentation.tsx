36c58bd328d63f5db244b89d57ab037ca9b874e3
Nacho <nachoballestrino02@gmail.com>
2024-11-13 12:21:59 -0300

    presentation & about-me

---
+++ components/presentation.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function Presentation() {
+  return (
+    <div className='flex md:flex-row h-screen flex-col-reverse w-full gap-20 justify-center md:justify-between items-center select-none'>
+      <div className='flex flex-col'>
+        <h1 className='text-8xl font-bold text-first hover:text-first/90 transition-colors duration-200 '>
+          Hi i&apos;m <br /> Nacho
+        </h1>
+        <p className='pl-1 text-2xl font-semibold'>Front End Developer</p>
+      </div>
+      <div>
+        <Image src={'/I.png'} alt='' width={250} height={250} />
+      </div>
+    </div>
+  )
+}
